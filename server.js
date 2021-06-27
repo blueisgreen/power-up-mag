f755073e6a41eaf799a47b30b3070625ac0d7f12
Dave Mount <davemount@gmail.com>
2021-06-27 11:35:49 -0700

    Enabling https

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 require('dotenv').config()
-const express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  cors = require('cors'),
-  port = process.env.PORT || 5000
-
+const express = require('express')
+const fs = require('fs')
+const https = require('https')
+const serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
+const cors = require('cors')
+const port = process.env.PORT || 5000
 const app = express()
 
 app.use(history())
 app.use(cors())
 app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(port)
-console.log(`Server started at http://localhost:${port}.`)
-console.log(`Base service URI: ${process.env.API_BASE}.`)
+
+const server = https.createServer(
+  {
+    key: fs.readFileSync('../local/server.key'),
+    cert: fs.readFileSync('../local/server.cert'),
+  },
+  app
+)
+server.listen(port, () => {
+  console.log(`Server listening over https at port ${port}.`)
+  console.log(`Base service URI: ${process.env.API_BASE}.`)
+})
